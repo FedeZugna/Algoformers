79c427820e53a6158b50856202f90e96e9e67c39
Nicolas <ncontinanza94@gmail.com>
2016-06-11 12:35:15 -0300

    Agrego Ataque, AtaqueEstandar y AtaquePorcentual

---
+++ src/fiuba/algo3/model/Ataque.java
@@ -0,0 +1,22 @@
+package model;
+
+public abstract class Ataque {
+
+	private int danio;
+
+	public Ataque(int danio) {
+		this.danio = danio;
+	}
+
+	public void ataqueEjecutar(Algoformer algoformer) {
+		algoformer.recibirAtaque(this.danio);
+	}
+
+	public int getDanio() {
+		return this.danio;
+	}
+	
+	public void setDanio(int danio){
+		this.danio = danio;
+	}
+}
